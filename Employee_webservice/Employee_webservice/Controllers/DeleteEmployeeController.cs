bbd8c34e1893932e992b401091bc2a842f92bc85
Shahzadi, Shamayla <Shamayla.Shahzadi@spglobal.com>
2019-01-16 16:59:54 +0500

    Delete Conroller Added

---
+++ Employee_webservice/Employee_webservice/Controllers/DeleteEmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Employee_webservice.Models;
+
+namespace Employee_webservice.Controllers
+{
+    public class DeleteEmployeeController : ApiController
+    {
+        [Route("employee/delete/{employeeID}")]
+
+        public String DeleteEmployee(String employeeID)
+        {
+            Console.WriteLine("In deleteEmployeeRecord");
+            return EmployeeRegistration.getInstance().Remove(employeeID);
+        }
+    }
+}
